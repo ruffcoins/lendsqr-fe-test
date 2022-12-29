429cb91ae53bc9e8963fe2a7dc2056849ce88bab
ihsidna <ihsidnadev@gmail.com>
2022-12-29 17:59:40 +0100

    feature(Login Page): Desktop view of the left side of the login page

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import LoginForm from './LoginForm'
 import Logo from '../../assets/logo.png'
+import Pablo from '../../assets/pablo.jpg'
 
 const Login = () => {
   return (
     <div className='login-page'>
-      <div>
-        <img src={Logo} alt='Logo' />
+
+      <div className="login-left-side">
+        <img src={Logo} alt='Logo' className='logo'/>
+        <img src={Pablo} alt='Pablo' className='pablo'/>
       </div>
-      <LoginForm />
+
+      <div className="login-right-side">
+       <LoginForm />
+      </div>
+
     </div>
   )
 }
